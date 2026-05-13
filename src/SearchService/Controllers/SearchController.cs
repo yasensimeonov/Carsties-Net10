8f3f910403a7fb2790f75b1967ee630391ae8882
Yasen Simeonov <y.simeonov@cnsys.bg>
2026-05-13 17:55:21 +0300

    feat: Completed Section 3, Chapter 28 `Paginating the Results`

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -9,9 +9,10 @@ namespace SearchService.Controllers;
 public class SearchController : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems(string? searchTerm)
+    public async Task<ActionResult<List<Item>>> SearchItems(string? searchTerm, int pageNumber = 1, int pageSize = 4)
     {
-        var query = DB.Find<Item>();
+        //var query = DB.Find<Item>();
+        var query = DB.PagedSearch<Item>();
 
         query.Sort(x => x.Ascending(a => a.Make));
 
@@ -20,9 +21,18 @@ public class SearchController : ControllerBase
             query.Match(Search.Full, searchTerm).SortByTextScore();
         }
 
+        query.PageNumber(pageNumber);
+        query.PageSize(pageSize);
+
         var result = await query.ExecuteAsync();
 
-        return result;
+        //return result;
+        return Ok(new
+        {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
     }
 
 }
